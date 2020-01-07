8d03cc42d1fbfb792ce168c5beb2e51151b2337e
Jan Richter <rich2@seznam.cz>
2020-01-07 22:26:24 +0100

    html support and better error handling

--- forwarder.go
+++ forwarder.go
@@ -22,50 +22,58 @@ func sendEmailWithMailgun(recipient string, body string) (string, string, error)
 	secretKey := os.Getenv("MAILGUN_PRIVATE_KEY")
 
 	sender := os.Getenv("EMAIL_SENDER")
-	subject := "Hi, someone sent you a message"
+	subject := os.Getenv("EMAIL_SUBJECT")
 
 	// Create an instance of the Mailgun Client
 	mg := mailgun.NewMailgun(domain, secretKey)
 
 	// The message object allows you to add attachments and Bcc recipients
-	message := mg.NewMessage(sender, subject, body, recipient)
+	message := mg.NewMessage(sender, subject, "", recipient)
+	message.SetHtml(body)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
 	// Send the message	with a 10 second timeout
 	return mg.Send(ctx, message)
-	//return resp, id, err
 }
 
-func isEmail(email string) bool {
-	var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-	fmt.Printf("error: '%s' mentioned in subject is not a valid email address\n", email)
+func isValidEmail(email string) bool {
+	rxEmail := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-	return len(email) > 254 || !rxEmail.MatchString(email)
+	return len(email) < 254 && rxEmail.MatchString(email)
 }
 
 func forwardEmail(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Post from website! subject = %v\n", r.FormValue("Subject"))
-	fmt.Printf("Post from website! body-html = %v\n", r.FormValue("body-html"))
-	fmt.Printf("Post from website! sender = %v\n", r.FormValue("sender"))
-	fmt.Printf("Post from website! To = %v\n", r.FormValue("To"))
+	fmt.Printf("subject = %v\n", r.FormValue("Subject"))
+	fmt.Printf("body-html = %v\n", r.FormValue("body-html"))
 
 	to := r.FormValue("Subject")
 	body := r.FormValue("body-html")
 
-	if to != "" && isEmail(to) && body != "" {
-		resp, id, err := sendEmailWithMailgun(to, body)
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is supported", http.StatusBadRequest)
+		return
+	}
 
-		if err != nil {
-			fmt.Fprintf(w, "Sorry, something wrong happened, please try again later")
-			log.Fatal(err)
+	if to != "" && body != "" {
+
+		if isValidEmail(to) {
+			resp, id, err := sendEmailWithMailgun(to, body)
+
+			if err != nil {
+				fmt.Fprintf(w, "Sorry, something went wrong, please try again later")
+			} else {
+				fmt.Fprintf(w, "Email forwarded!")
+				fmt.Printf("ID: %s Resp: %s\n", id, resp)
+			}
+
+		} else {
+			fmt.Printf("error: '%s' mentioned in the subject is not a valid email address\n", to)
+			http.Error(w, "Invalid recepient email", http.StatusBadRequest)
 		}
-
-		fmt.Fprintf(w, "Email forwarded!")
-		fmt.Printf("ID: %s Resp: %s\n", id, resp)
 	} else {
-		fmt.Fprintf(w, "Missing or invalid recepient email")
+		http.Error(w, "Missing params", http.StatusBadRequest)
 	}
 }
 
